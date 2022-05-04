afa9424f1e96f49b5a05724825eb10332b979e28
Aladdin Wattar <aladdin.wattar@atechytechnologies.com>
2022-05-05 01:25:25 +0400

    enabled optimizer

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -17,67 +17,64 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
- const path = require('path');
- const filePath = path.join(__dirname, '../.env');
- require('dotenv').config({ path: filePath });
+const path = require("path");
+const filePath = path.join(__dirname, "../.env");
+require("dotenv").config({ path: filePath });
 
- const HDWalletProvider = require('@truffle/hdwallet-provider');
- const verify = require('truffle-plugin-verify');
- 
- module.exports = {
-  
-   networks: {
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const verify = require("truffle-plugin-verify");
 
-     development: {
-      host: '127.0.0.1',
-      port: 7545, 
-      network_id: 5777, 
-      },
-     rinkeby: {
-       provider: () =>
-         new HDWalletProvider(
-           `${process.env.Deployer_Primary_key}`,
-           `${process.env.ProvideRinkeby}`
-         ),
-       network_id: 4, 
-       gas: 7500000, 
-       confirmations: 2, 
-       timeoutBlocks: 200, 
-       skipDryRun: true, 
-     },
-     mumbai: {
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: 5777,
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          `${process.env.Deployer_Primary_key}`,
+          `${process.env.ProvideRinkeby}`
+        ),
+      network_id: 4,
+      gas: 7500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    mumbai: {
       provider: () =>
         new HDWalletProvider(
           `${process.env.Deployer_Primary_key}`,
           `${process.env.ProviderMumbai}`
         ),
-      network_id: 80001, 
-      gas: 5500000, 
+      network_id: 80001,
+      gas: 5500000,
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true,
     },
-   },
-   plugins: ['truffle-plugin-verify'],
-   api_keys: {
-     etherscan: `${process.env.Etherscan_Key}`,
-     polygonscan:`${process.env.Polygonscan_Key}`
-   },
-   // Configure your compilers
-   compilers: {
-     solc: {
-       version: '^0.8.0',
-       //version: "0.6.2",
-       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-       //  optimizer: {
-       //    enabled: false,
-       //    runs: 200
-       //  },
-       //  evmVersion: "byzantium"
-       // }
-     },
-   },
- };
- 
+  },
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    etherscan: `${process.env.Etherscan_Key}`,
+    polygonscan: `${process.env.Polygonscan_Key}`,
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "^0.8.0",
+      //version: "0.6.2",
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      optimizer: {
+        enabled: false,
+        runs: 200,
+      },
+      //  evmVersion: "byzantium"
+      // }
+    },
+  },
+};
